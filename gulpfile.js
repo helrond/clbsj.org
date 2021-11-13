1f493c93186806e65ba0ee501601295660e6c5b9
Hillel Arnold <helrond@hotmail.com>
2021-11-13 16:40:51 -0500

    update infrastructure

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,21 @@
-var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+'use strict'
+var gulp = require('gulp')
+var sass = require('gulp-sass')(require('sass'))
 
 var sassPaths = [
   'bower_components/normalize.scss/sass',
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
-];
+]
 
-function sass() {
+function buildStyles () {
   return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
-}
+    .pipe(sass(
+      {
+        includePaths: sassPaths,
+        outputStyle: 'compressed' // if css compressed **file size**
+      }).on('error', sass.logError))
+    .pipe(gulp.dest('css'))
+};
 
-exports.default = (sass)
+exports.default = buildStyles
